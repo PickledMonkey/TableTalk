48a574d529c73740426573b75e7bc7ff2de163b2
Paul Graff <banana.manwich@gmail.com>
2018-04-02 14:35:46 -0400

    Added some basic functionality for switching pages

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,9 +2,14 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -20,12 +25,33 @@ gulp.task('js', function() {
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-    browserSync.init({
-        server: "./src"  
-    });
+    // browserSync.init({
+    //     server: "./src"  
+    // });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    // gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    // gulp.watch("src/*.html").on('change', browserSync.reload);
+    app.use(express.static(__dirname + '/src'));
+
+    app.get('/', function(req, res){
+	  	res.sendFile(__dirname + '/src/index.html');
+	});
+
+	app.post('/playerConversations/', function (req, res) {
+		// const body = req.body.Body
+		// res.set('Content-Type', 'text/plain')
+		// res.send(`You sent: ${body} to Express`)
+
+		res.sendFile(__dirname + '/src/html/conversations.html');
+	})
+
+	app.get('/playerConversations/', function(req, res){
+	  	res.sendFile(__dirname + '/src/html/conversations.html');
+	});
+
+	http.listen(3000, function(){
+	  console.log('listening on *:3000');
+	});
 });
 
 gulp.task('default', ['js','serve']);
---
+++ server/src/js/homepage_handler.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    
+    // $("#playerForm").submit(function(){
+    //     alert("Player");
+
+    //     $("#userInputForm").load("/src/html/playerForm.html");
+
+    // });
+
+    $("#playerSelectButton").click(function(){
+        $("#userInputForm").load("html/playerForm.html");
+    });
+
+    $("#dmSelectButton").click(function(){
+        $("#userInputForm").load("html/dungeonMasterForm.html");
+    });
+    
+});
